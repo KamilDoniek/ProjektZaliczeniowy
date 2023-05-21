4f61db2fe64ce9cee1f99669d2b12d1f18a5887a
KamilDoniek <kamil.doniek@microsoft.wsei.edu.pl>
2023-05-21 13:29:27 +0200

    commit

--- script/script.js
+++ script/script.js
@@ -14,9 +14,4 @@ hamburger.addEventListener("click",burgerClick,false);
 const skill_inner =document.querySelector('.skill_inner');
 const skill = document.querySelector('.skill');
 
-const skillSkroll = ()=> {
-    skill_inner.classList.toggle('skill_inner--scroll');
-}
-
-skill.addEventListener("scroll",skillSkroll,false);
 
